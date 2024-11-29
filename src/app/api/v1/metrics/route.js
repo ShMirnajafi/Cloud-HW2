e84d77a5928f522f8cf892cde0828a8bb3c17fa0
mirnajafizade <mirnajafizade@mahsan.co>
2024-11-29 09:37:29 +0330

    add prometheus

--- src/app/api/v1/metrics/route.js
+++ src/app/api/v1/metrics/route.js
@@ -3,7 +3,15 @@ import client from 'prom-client';
 
 const register = client.register;
 
+const requestCounter = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'status_code'],
+});
+
 export async function GET() {
+    requestCounter.inc({ method: 'GET', status_code: '200' });
+
     const metrics = await register.metrics();
     return new NextResponse(metrics, {
         headers: { 'Content-Type': register.contentType }
